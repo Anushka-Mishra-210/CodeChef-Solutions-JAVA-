f55d07df2b9028a5c1e7a68243a26df1d7443daf
Anushka Mishra <99817053+Anushka-Mishra-210@users.noreply.github.com>
2022-03-22 19:29:40 +0530

    Add files via upload

---
+++ JGAMES.java
@@ -0,0 +1,27 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int T = sc.nextInt();
+	    
+	    if(T>=1 && T<=100)
+	    {
+		for(int i =1; i<=T;i++)
+		{
+		int X = sc.nextInt();
+		int Y = sc.nextInt();
+		
+	    if((X+Y)%2==0)
+	    System.out.println("JANMANSH");
+	    else 
+	    System.out.println("JAY");}}
+	}
+}
