f867cdd790d1cc7954aed6b68ec3883bfa9a207c
Anushka Mishra <99817053+Anushka-Mishra-210@users.noreply.github.com>
2022-04-03 00:26:21 +0530

    Add files via upload

---
+++ VOLCONTROL.java
@@ -0,0 +1,33 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner (System.in);
+		int T = sc.nextInt();
+		
+		if(T>=1 && T<=100)
+		{
+		    for(int i=1; i<=T; i++)
+		    {
+		        int X = sc.nextInt();
+		        int Y = sc.nextInt();
+		        
+		        if(X>Y)
+		        {
+		           System.out.println(X-Y);
+		        }
+		        else 
+		        System.out.println(Y-X);
+		    }
+		}
+		
+		// your code goes here
+	}
+}
