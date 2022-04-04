db34ecd0c221e345101993fcf8b0e82034c70d9b
Anushka Mishra <99817053+Anushka-Mishra-210@users.noreply.github.com>
2022-04-04 23:21:41 +0530

    Add files via upload

---
+++ CHEFPAGES.java
@@ -0,0 +1,25 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+       if (A==1 && B==1)
+		{System.out.println("https://discuss.codechef.com");}
+		else if (A==1 && B==0)
+		{System.out.println("https://www.codechef.com/contests");}
+		else 
+		{System.out.println("https://www.codechef.com/practice");}
+		
+		// your code goes here
+	}
+}
