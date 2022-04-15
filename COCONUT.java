2b549513f866728cd40fe57033ab8cd27f583078
Anushka Mishra <99817053+Anushka-Mishra-210@users.noreply.github.com>
2022-04-16 01:01:19 +0530

    Add files via upload

---
+++ COCONUT.java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  Scanner sc =new Scanner(System.in);
+	  int T = sc.nextInt();
+	  
+	  for(int i =1; i<=T; i++)
+	  {
+	      int xa = sc.nextInt();
+	      int xb = sc.nextInt();
+	      int XA = sc.nextInt();
+	      int XB = sc.nextInt();
+	      
+	      System.out.println(XA/xa+XB/xb);
+	  }
+	
+	
+		// your code goes here
+	}
+}
